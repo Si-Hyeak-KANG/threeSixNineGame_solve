6def23624d9ad3d0d62e0f8fd1758a7230e52f9f
James Kang <79829085+Si-Hyeak-KANG@users.noreply.github.com>
2024-02-14 15:18:53 +0900

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -1,22 +1,21 @@
+//이름과 오답율을 가지고 있는 클래스 만들기
 public class Player {
-    //이름과 오답율을 가지고 있는 클래스 만들기
     String name;
-    int errorRate; // % 저장
+    double errorRate; // % 저장
 
     protected Player(){}
 
-    private Player(String name, int errorRate) {
+    private Player(String name, double errorRate) {
         this.name = name;
         this.errorRate = errorRate;
     }
 
-    static Player of(String name, int errorRate) {
+    static Player of(String name, double errorRate) {
         return new Player(name, errorRate);
     }
 
     public boolean correctAnswer() {
-        double v = Math.random() * 100;
-
-        return v <= errorRate;
+        double randomValue = Math.random() * 100; 
+        return randomValue <= errorRate;
     }
 }
